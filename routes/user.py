5527b9a367a4920809d61f10f57feb02ee7e8c71
Ravshanbek <rasidsobirov38@gmail.com>
2025-06-07 21:49:37 +0500

    Test

--- routes/user.py
+++ routes/user.py
@@ -26,9 +26,9 @@ async def get_all_users(role: str = None, page: int = 1, limit: int = 25, \
     filter_query = and_(*filters) if filters else None
     return await get_all(db=db, model=User, filter_query=filter_query, page=page, limit=limit)
 
-# @user_router.get("/users/{phone_number}", summary="Foydalanuvchi haqida ma'lumot olish")
-# async def get_user(phone_number: int, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_active_user)) -> UserResponse:
-#     return await get_one(db=db, model=User, filter_query=(User.phone_number==phone_number))
+@user_router.get("/users/{phone_number}", summary="Foydalanuvchi haqida ma'lumot olish")
+async def get_user(phone_number: int, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_active_user)) -> UserResponse:
+    return await get_one(db=db, model=User, filter_query=(User.phone_number==phone_number))
 
 @user_router.post("/users/create", summary="Foydalanuvchi yaratish")
 async def create_user(form: UserCreateForm, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_active_user)) -> UserCreateForm:
