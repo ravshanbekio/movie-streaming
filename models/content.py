1868fa8e898de4b77133144d5cad46990e78b44e
Ravshanbek <rasidsobirov38@gmail.com>
2025-06-11 23:17:20 +0500

    Added GET Content, changed playlist detail route

--- models/content.py
+++ models/content.py
@@ -10,18 +10,6 @@ movie_genre_association = Table(
     Column('genre_id', ForeignKey('genres.genre_id'), primary_key=True)
 )
 
-class Playlist(Base):
-    __tablename__ = "playlists"
-
-    playlist_id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(255))
-    description = Column(Text, nullable=True)
-    thumbnail = Column(String(255))
-    created_at = Column(DateTime)
-
-    # Relationships
-    contents = relationship("Content", back_populates="playlist")
-
 class Content(Base):
     __tablename__ = "contents"
 
